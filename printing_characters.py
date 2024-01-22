50dc5ec03cfb2f9ffe002397bd479b54db429ead
maihannn <maicapalillo15@gmail.com>
2024-01-22 20:13:36 +0800

    done, it worked

--- printing_characters.py
+++ printing_characters.py
@@ -4,9 +4,15 @@
 # Exercise 3: Print characters from a string that are present at an even index number.
 
 # Create input to accept a string from user
+
+input_word = str (input ("Enter a word: "))
+len (input_word)
+
 # Get only the characters present at an even index number
+for i in range (len(input_word)):
+    if (i % 2):
+        print (input_word [i])
 
-name = str(input("Enter a word: "))
 
-for i in range (name):
-    print (name[i])
+            
+
